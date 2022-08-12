e69e831b500c475e02959fcc0771bf895488b1a3
Jimy <jimenezmota15@gmail.com>
2022-08-12 17:06:50 +0200

    included add tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-
-import TaskList from "./TaskList";
-import TaskForm from "./TaskForm";
-
+import { tasks as data } from "./data/tasks";
+import { useEffect, useState } from "react";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask({title, description}) {
+    setTasks([...tasks,{
+      title,
+      id: tasks.length,
+      description
+    }]);
+  }
 
   return (
     <>
-      <TaskForm />
-      <TaskList />
+      <TaskForm createTask={createTask} />
+      <hr />
+      <TaskList tasks={tasks} />
     </>
   );
 }
