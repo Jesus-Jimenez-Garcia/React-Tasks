d9fabe8a1e3a3def8166bff978f319137d0a72a1
Jimy <jimenezmota15@gmail.com>
2022-08-13 20:20:08 +0200

    sorted all elements in the context

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { tasks as data } from "./data/tasks";
-import { useEffect, useState } from "react";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
-  function createTask({ title, description }) {
-    setTasks([
-      ...tasks,
-      {
-        title,
-        id: tasks.length,
-        description,
-      },
-    ]);
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  }
-
   return (
     <>
-      <TaskForm createTask={createTask} />
+      <TaskForm />
       <hr />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
+      <TaskList />
     </>
   );
 }
